f747ab04ae5c55ad313529c26625e5e6cda105e4
unknown <fovakieleni@gmail.com>
2024-06-27 14:29:28 +0300

    Non primitive data type: Strings
    Non primitive data type: Strings

--- java tutorial/src/Main.java
+++ java tutorial/src/Main.java
@@ -1,24 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-
-        int myNumber; //holds integers
-        myNumber = 88; //initialization
-        System.out.println(myNumber);
-        short myShort = 847;
-        System.out.println(myShort);
-        long myLong = 9797;
-        System.out.println(myLong);
-        double myDouble = 7.3243;
-        System.out.println(myDouble);
-        float myFloat = 324.3f; //needs the f in the end to be a float
-        System.out.println(myFloat);
-        char myChar = 'y';
-        System.out.println(myChar);
-        boolean myBoolean = true;
-        System.out.println(myBoolean);
-        byte myByte = 127; //holds 8bit data
-        System.out.println(myByte);
+        String text = "Hello world!"; // non primitive data type, is a class
+        String blank = "";
+        String name = "Bob";
+        String greeting = text + blank + name;
+        System.out.println(greeting);
+        System.out.println("Hello" + " " + "Bob");
+        double myDouble = 7.8;
+        System.out.println("My number is " +myDouble);
     }
 }

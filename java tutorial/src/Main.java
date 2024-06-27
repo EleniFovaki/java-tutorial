461d9cc499e677aee5729a41ffedf9708e1b5e85
unknown <fovakieleni@gmail.com>
2024-06-27 14:15:38 +0300

    Primitive Data Types
    Primitive Data Types

---
+++ java tutorial/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        int myNumber; //holds integers
+        myNumber = 88; //initialization
+        System.out.println(myNumber);
+        short myShort = 847;
+        System.out.println(myShort);
+        long myLong = 9797;
+        System.out.println(myLong);
+        double myDouble = 7.3243;
+        System.out.println(myDouble);
+        float myFloat = 324.3f; //needs the f in the end to be a float
+        System.out.println(myFloat);
+        char myChar = 'y';
+        System.out.println(myChar);
+        boolean myBoolean = true;
+        System.out.println(myBoolean);
+        byte myByte = 127; //holds 8bit data
+        System.out.println(myByte);
+    }
+}

42cf77ab10aada2ea51f6bb694c962d7f082d242
unknown <fovakieleni@gmail.com>
2024-06-27 15:31:46 +0300

    If conditional statement
    If conditional statement

--- java tutorial/src/Main.java
+++ java tutorial/src/Main.java
@@ -1,7 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        for(int i = 0; i < 5; i++){
-            System.out.printf("The value of i is: %d\n",i);
+        int myInt = 30;
+        if(myInt < 10){
+            System.out.println("Yes, it's true");
         }
+        else if(myInt > 20){
+            System.out.println("No, it's false");
+        }
+        else {
+            System.out.println("None of the above");
+        }
+
+        int loop = 0;
+        while(true){
+            System.out.println("Looping:"+loop);
+            if(loop==5){
+                break;
+            }
+            loop++;
+            System.out.println("Running");
+        }
+
     }
 }

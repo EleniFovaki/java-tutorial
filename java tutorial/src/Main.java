2cd7757e58781da99345fd076c42497d3ca9694f
unknown <fovakieleni@gmail.com>
2024-06-27 15:05:12 +0300

    while loops
    while loops

--- java tutorial/src/Main.java
+++ java tutorial/src/Main.java
@@ -1,13 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        String text = "Hello world!"; // non primitive data type, is a class
-        String blank = "";
-        String name = "Bob";
-        String greeting = text + blank + name;
-        System.out.println(greeting);
-        System.out.println("Hello" + " " + "Bob");
-        double myDouble = 7.8;
-        System.out.println("My number is " +myDouble);
+        int value = 0;
+       while (value < 10) { //condition
+           System.out.println("Hello" + value);
+           value = value + 1;
+       }
     }
 }

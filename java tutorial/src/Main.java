b6caf0f93f3b71906c0edc6ac761587c1ac9d4ba
unknown <fovakieleni@gmail.com>
2024-06-28 16:18:31 +0300

    Methods
    Methods

--- java tutorial/src/Main.java
+++ java tutorial/src/Main.java
@@ -1,6 +1,13 @@
 class Person {
+    //Classes can contain:
     String name; //instance variables
     int age; //instance variables
+
+    //Subroutines(methods)
+    void speak(){
+        System.out.println("My name is " + name + " and I am " + age + " years old.");
+    }
+
 }
 
 public class Main {
@@ -8,11 +15,11 @@ public class Main {
         Person person1 = new Person();
         person1.name = "Joe Blogs";
         person1.age = 31;
+        person1.speak();
 
         Person person2 = new Person();
         person2.name = "Sarah Smith";
         person2.age = 32;
-
-        System.out.println(person1.name);
+        person2.speak();
     }
 }

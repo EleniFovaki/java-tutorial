470f10f8dde287c6a7375468b0bb75af8c50a28e
unknown <fovakieleni@gmail.com>
2024-06-28 11:53:59 +0300

    Do while loop
    Do while loop

--- java tutorial/src/Main.java
+++ java tutorial/src/Main.java
@@ -1,25 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int myInt = 30;
-        if(myInt < 10){
-            System.out.println("Yes, it's true");
+        Scanner input = new Scanner(System.in);
+        int value = 0;
+        do {
+            System.out.println("Enter a number");
+            value = input.nextInt();
         }
-        else if(myInt > 20){
-            System.out.println("No, it's false");
-        }
-        else {
-            System.out.println("None of the above");
-        }
-
-        int loop = 0;
-        while(true){
-            System.out.println("Looping:"+loop);
-            if(loop==5){
-                break;
-            }
-            loop++;
-            System.out.println("Running");
-        }
-
+        while (value != 5);
+        System.out.println("Got 5!");
     }
 }

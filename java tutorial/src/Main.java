e94c27deff5f9bdd871820c7618c7319eca40043
unknown <fovakieleni@gmail.com>
2024-06-28 12:41:18 +0300

    Arrays
    Arrays

--- java tutorial/src/Main.java
+++ java tutorial/src/Main.java
@@ -2,19 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter a command");
-        String text = input.nextLine();
+        int value = 7;
+        int[] values;
 
-        switch (text){
-            case "start":
-                System.out.println("Machine started");
-                break;
-            case "stop":
-                System.out.println("Machine stopped");
-                break;
-            default:
-                System.out.println("Command not recognized");
+        values = new int[3]; //new keyword allocates the memory
+
+        System.out.println(values[0]); //prints 0 if the value is not defined in this position
+        for(int i =0; i<values.length; i++){ //iterates through the array
+            System.out.println(values[i]);
+        }
+        int[] numbers ={5,6,7};
+        for (int i=0; i<numbers.length; i++){
+            System.out.println(numbers[i]);
         }
     }
 }

aa067de8810933bcce5da9ee91fcebd2fb9c9f96
unknown <fovakieleni@gmail.com>
2024-06-28 15:50:44 +0300

    Classes
    Classes

--- java tutorial/src/Main.java
+++ java tutorial/src/Main.java
@@ -1,22 +1,18 @@
-import java.util.Scanner;
+class Person {
+    String name; //instance variables
+    int age; //instance variables
+}
 
 public class Main {
     public static void main(String[] args) {
-        int[][] grid = {
-                {1, 2, 3}, //0x0, 0x1, 0x2
-                {3, 2, 1}, //1x0, 1x1, 1x2
-                {5, 6, 7} //2x0, 2x1, 2x2
-        };
-        System.out.println(grid[1][1]); //row x column
+        Person person1 = new Person();
+        person1.name = "Joe Blogs";
+        person1.age = 31;
 
-        String[][] texts = new String[2][3];
-        texts[0][1] = "hello there";
-        System.out.println(texts[0][1]);
-        for(int row=0; row<grid.length; row++){
-            for(int col=0; col< grid[row].length; col++){
-                System.out.print(grid[row][col] + "\t");
-            }
-            System.out.println();
-        }
+        Person person2 = new Person();
+        person2.name = "Sarah Smith";
+        person2.age = 32;
+
+        System.out.println(person1.name);
     }
 }

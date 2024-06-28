a1861f623f90d548520e1ce16828f0798d610906
unknown <fovakieleni@gmail.com>
2024-06-28 12:29:32 +0300

    Switch case
    Switch case

--- java tutorial/src/Main.java
+++ java tutorial/src/Main.java
@@ -3,12 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int value = 0;
-        do {
-            System.out.println("Enter a number");
-            value = input.nextInt();
+        System.out.println("Enter a command");
+        String text = input.nextLine();
+
+        switch (text){
+            case "start":
+                System.out.println("Machine started");
+                break;
+            case "stop":
+                System.out.println("Machine stopped");
+                break;
+            default:
+                System.out.println("Command not recognized");
         }
-        while (value != 5);
-        System.out.println("Got 5!");
     }
 }

e975719bc0b4a5c9139e26a64fc4df19c98b60cf
unknown <fovakieleni@gmail.com>
2024-06-30 15:41:56 +0300

    Getters and return values
    Getters and return values

--- java tutorial/src/Main.java
+++ java tutorial/src/Main.java
@@ -5,9 +5,19 @@ class Person {
 
     //Subroutines(methods)
     void speak(){
+
         System.out.println("My name is " + name + " and I am " + age + " years old.");
     }
-
+    int calculateYearsToRetirement(){ //returns a value
+        int yearsLeft = 65 - age;
+        return yearsLeft;
+    }
+    String getName(){ //getters
+        return name;
+    }
+    int getAge(){
+        return age;
+    }
 }
 
 public class Main {
@@ -16,10 +26,18 @@ public class Main {
         person1.name = "Joe Blogs";
         person1.age = 31;
         person1.speak();
+        int years = person1.calculateYearsToRetirement();
+        System.out.println("Years to retire: " + years);
+        int age = person1.getAge();
+        String name = person1.getName();
 
         Person person2 = new Person();
         person2.name = "Sarah Smith";
         person2.age = 32;
         person2.speak();
+        int age2 = person2.getAge();
+        String name2 = person2.getName();
+        System.out.println("Name is: " + name2);
+        System.out.println("Age is: " + age2);
     }
 }

c48a57e7196e1fb993f818675df5401f79ce865c
unknown <fovakieleni@gmail.com>
2024-07-08 21:08:43 +0300

    Static (and final keyword)
    Static (and final keyword)

--- java tutorial/src/Main.java
+++ java tutorial/src/Main.java
@@ -1,21 +1,32 @@
-class Machine {
-    private String name;
-    private int age;
+class Thing {
+    public final static int LUCKY_NUMBER = 5;
+    public String name;
+    public static String description;
 
-    public Machine() { //this is how you define a constructor
-        System.out.println("A machine is running"); //so this runs automatically everytime an instance of a class is created
+    public void showName(){
+        System.out.println(description + ": " + name);
     }
-    public Machine(String name, int age) { //or a constructor can act as a getter and a setter method combined
-        this.name = name;
-        this.age = age;
-        System.out.println("the machine is called: " + name + age);
+    public static void showInfo(){
+        System.out.println(description);
     }
 }
 
 public class Main {
     public static void main(String[] args) {
+        Thing.description = "I am a thing";
+        Thing.showInfo();
 
-        Machine c3po = new Machine("c3po", 5); //the output depends on the arguments of the constructor
+        Thing thing1 = new Thing();
+        Thing thing2 = new Thing();
+
+        thing1.name = "Bob";
+        thing2.name = "Sue";
+
+        thing1.showName();
+        thing2.showName();
+
+        System.out.println(Math.PI);
+        System.out.println(Thing.LUCKY_NUMBER);
     }
 
 }
